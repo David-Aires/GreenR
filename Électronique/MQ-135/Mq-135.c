761d53ba9c17358723f3633be93927f6fed4bfaa
Vanden Herrewegen Guillaume <32937851+guivdh@users.noreply.github.com>
2019-09-30 17:42:56 +0200

    Update Mq-135.c

--- Électronique/MQ-135/Mq-135.c
+++ Électronique/MQ-135/Mq-135.c
@@ -1,4 +1,3 @@
-#include <dht.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <MQ135.h>
@@ -7,10 +6,6 @@ LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars
 
 const int mq135pin = A1;
 MQ135 gasSensor = MQ135(mq135pin);
-
-#define dht_dpin A0
-
-dht DHT;
 
 void setup() {
   
@@ -39,22 +34,10 @@ void loop() {
 
   float ppm = gasSensor.getPPM();
   
-  DHT.read11(dht_dpin);
-  Serial.print("Humidité=");
-  Serial.print(DHT.humidity);
-  Serial.print("% ");
-  Serial.print("Température= ");
-  Serial.print(DHT.temperature);
-  Serial.print("°C ");
   Serial.print("AirQua=");
   Serial.print(ppm);               // prints the value read
   Serial.println(" PPM");
 
-  lcd.setCursor(0,0);
-  lcd.print(DHT.humidity);
-  lcd.print(" % - ");
-  lcd.print(DHT.temperature);
-  lcd.print("*C");
   lcd.setCursor(0,1);
   lcd.print(ppm);
   lcd.print("ppm");
