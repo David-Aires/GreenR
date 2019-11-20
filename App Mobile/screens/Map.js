ae7ea79b1210cabb911e8ef68dfefc4a07462db1
rebald <43856164+rebald@users.noreply.github.com>
2019-11-20 01:01:52 +0100

    change map

--- App Mobile/screens/Map.js
+++ App Mobile/screens/Map.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import MapView from 'react-native-maps';
 import {  AppRegistry,
@@ -11,18 +10,12 @@ import {  AppRegistry,
   Dimensions, } from 'react-native';
 
   import markerImg from '../assets/airbox_icon.png';
-  import { createAppContainer } from 'react-navigation';
-  import { createStackNavigator } from 'react-navigation-stack';
 
 export default class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state ={ coord : []}
-  }
-
   componentDidMount(){
-    return fetch ('https://green-r.be/api/stats.php?position=ALL')
+    //return fetch('https://facebook.github.io/react-native/movies.json')
+    return fetch ('https://green-r.be/api/stats.php?box=1&table=AIR_STAT')
       .then((response) => response.json())
       .then((responseJson) => {
 
@@ -39,79 +32,154 @@ export default class App extends React.Component {
       });
   }
 
-  renderCoo = () =>{
-    return (
 
-      <View style={styles.container}>
-        <MapView style={styles.map}
-          initialRegion={{
+  constructor(props){
+    super(props);
+
+
+
+
+    this.state={
+
+      isLoading: true,
+
+      marker1: [
+        {
+          coordinate: {
             latitude: 50.665791,
             longitude: 4.612230,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-          }}
-          zoomEnabled={true}
-          style={styles.mapStyle}
-        >
-
-        {this.state.coord.map(marker => {
-      return (
-        <MapView.Marker key={index}
-        coordinate={ {longitude: {marker.LON}},{latitude: {marker.LAT}} }
-        //title={this.state.markerA.title}
-        //description={this.state.markerA.description}
-        image={markerImg}
-        >
-          <Animated.View style={[styles.markerWrap]}>
-            <Animated.View style={[styles.ring]} />
-            <View style={styles.marker} />
-          </Animated.View>
-        </MapView.Marker>
-      );
-    })}
-      </MapView>
- </View>
-    );
+          },
+          title: "EPHEC",
+          description: "Etat actuel: Bon\n Température: 12°C\n Humidité: 62%",
+        },
+      ],
+      marker2: [
+        {
+          coordinate: {
+            latitude: 50.667003,
+            longitude: 4.616839,
+          },
+          title: "Martin V",
+          description: "Etat actuel: Très bon\n Température: 12.5°C\n Humidité: 55%",
+        },
+      ],
+      region: {
+        latitude: 50.665791,
+        longitude: 4.612230,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      },
+    }
   }
 
-
-  //   this.state={
-  //     markerA: [
-  //       {
-  //         coordinate: {
-  //           latitude: 50.665791,
-  //           longitude: 4.612230,
-  //         },
-  //         title: "EPHEC",
-  //         description: "Etat actuel: Bon\n Température: 12°C\n Humidité: 62%",
-  //       },
-  //     ],
-  //     markerB: [
-  //       {
-  //         coordinate: {
-  //           latitude: 50.667003,
-  //           longitude: 4.616839,
-  //         },
-  //         title: "Martin V",
-  //         description: "Etat actuel: Très bon\n Température: 12.5°C\n Humidité: 55%",
-  //       },
-  //     ],
-  //     region: {
-  //       latitude: 50.665791,
-  //       longitude: 4.612230,
-  //       latitudeDelta: 0.005,
-  //       longitudeDelta: 0.005,
-  //     },
-  //   }
-  // }
-
   render() {
-    return (
-      <Block>
-        {this.renderCoo()}
-      </Block>
-    );
 
+    /*
+         
+    state = {
+      markers: [
+        {
+          coordinate: {
+            latitude: 50.665791,
+            longitude: 4.612230,
+          },
+          title: "EPHEC",
+          description: "SCHOOL",
+        },
+      ],
+      region: {
+        latitude: 50.665791,
+        longitude: 4.612230,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      },
+    };
+
+     <MapView.Marker
+            coordinate={this.state.marker1.latidude, this.state.marker1.longitude}
+            title={this.state.marker1.title}
+            description={this.state.marker1.description}
+            image={markerImg}
+         />
+
+
+         <MapView.Marker
+            coordinate={this.state.marker2.coordinate}
+            title={this.state.marker2.title}
+            description={this.state.marker2.description}
+            image={markerImg}
+         />
+      */
+
+    return (
+      
+        <View style={styles.container}>
+          <MapView style={styles.map}
+            initialRegion={{
+              latitude: this.state.region.latitude,
+              longitude: this.state.region.longitude,
+              latitudeDelta: this.state.region.latitudeDelta,
+              longitudeDelta: this.state.region.longitudeDelta,
+            }}
+            zoomEnabled={true}
+            style={styles.mapStyle}
+
+
+
+            data={this.state.dataSource}
+            renderItem={({item}) => 
+            <Text>
+            
+            {item.ID_AIR}, 
+            {item.TEMPERATURE},
+            {item.HUMIDITY},
+            {item.CO2},
+            {item.DATE}
+            
+            </Text>}
+            keyExtractor={({ID_AIR}, index) => ID_AIR}
+
+
+          >
+          
+          {this.state.marker1.map((marker, index) => {
+        return (
+          <MapView.Marker key={index} 
+          coordinate={marker.coordinate}
+          title={this.state.marker1.title}
+          description={this.state.marker1.description}
+          image={markerImg}
+          >
+            <MapView.Callout tooltip style={styles.customView}>
+                <View style={styles.calloutText}>
+                  <Text>{marker.title}{"\n"}{marker.description}</Text>
+                </View>
+            </MapView.Callout>
+
+          </MapView.Marker>
+        );
+      })}
+
+          {this.state.marker2.map((marker, index) => {
+        return (
+          <MapView.Marker key={index} 
+          coordinate={marker.coordinate}
+          title={this.state.marker2.title}
+          description={this.state.marker2.description}
+          image={markerImg}
+          >
+            <MapView.Callout tooltip style={styles.customView}>
+                <View style={styles.calloutText}>
+                  <Text>{marker.title}{"\n"}{marker.description}</Text>
+                </View>
+            </MapView.Callout>
+          </MapView.Marker>
+        );
+      })}
+        </MapView>
+  </View>
+      
+    );
   }
 }
 
@@ -129,135 +197,3 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
   },
 });
-=======
-import React from 'react';
-import MapView from 'react-native-maps';
-import {  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Animated,
-  Image,
-  Dimensions, } from 'react-native';
-
-  import markerImg from '../assets/airbox_icon.png';
-  import { createAppContainer } from 'react-navigation';
-  import { createStackNavigator } from 'react-navigation-stack';
-
-export default class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state ={ coord : []}
-  }
-
-  componentDidMount(){
-    return fetch ('https://green-r.be/api/stats.php?box=1&table=POSITION&relever=dernier')
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
-
-  renderCoo = () =>{
-    return (
-
-      <View style={styles.container}>
-        <MapView style={styles.map}
-          initialRegion={{
-            latitude: 50.665791,
-            longitude: 4.612230,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-          }}
-          zoomEnabled={true}
-          style={styles.mapStyle}
-        >
-
-        {this.state.coord.map((marker,index) => {
-      return (
-        <MapView.Marker key={index}
-        coordinate= {longitude: {this.state.coord.LON},latitude: {this.state.coord.LAT}} 
-        //title={this.state.markerA.title}
-        //description={this.state.markerA.description}
-        image={markerImg}
-        >
-          <Animated.View style={[styles.markerWrap]}>
-            <Animated.View style={[styles.ring]} />
-            <View style={styles.marker} />
-          </Animated.View>
-        </MapView.Marker>
-      );
-    })}
-      </MapView>
-    </View>
-    );
-  }
-
-
-  //   this.state={
-  //     markerA: [
-  //       {
-  //         coordinate: {
-  //           latitude: 50.665791,
-  //           longitude: 4.612230,
-  //         },
-  //         title: "EPHEC",
-  //         description: "Etat actuel: Bon\n Température: 12°C\n Humidité: 62%",
-  //       },
-  //     ],
-  //     markerB: [
-  //       {
-  //         coordinate: {
-  //           latitude: 50.667003,
-  //           longitude: 4.616839,
-  //         },
-  //         title: "Martin V",
-  //         description: "Etat actuel: Très bon\n Température: 12.5°C\n Humidité: 55%",
-  //       },
-  //     ],
-  //     region: {
-  //       latitude: 50.665791,
-  //       longitude: 4.612230,
-  //       latitudeDelta: 0.005,
-  //       longitudeDelta: 0.005,
-  //     },
-  //   }
-  // }
-
-  render() {
-    return (
-      <Block>
-        {this.renderCoo()}
-      </Block>
-    );
-
-  }
-}
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
->>>>>>> Stashed changes
