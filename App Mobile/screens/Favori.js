fef9b14c57c269217691e76b7dec040c6d6beab2
rebald <43856164+rebald@users.noreply.github.com>
2019-12-03 23:48:13 +0100

    changement favori

--- App Mobile/screens/Favori.js
+++ App Mobile/screens/Favori.js
@@ -15,49 +15,96 @@ import { Images, argonTheme } from "../constants";
 
 class Favori extends React.Component {
 
+  _isMounted = false;
+
   constructor(props){
     super(props);
     this.state ={ isLoading: true}
   }
 
-  componentDidMount(){
-    return fetch ('https://green-r.be/api/stats.php?position=ALL')
+componentDidMount(){
+  this._isMounted = true;
+  this.catchAPIdata();
+}
+
+
+componentWillUnmount() {
+  this._isMounted = false;
+}
+
+
+catchAPIdata= () =>{
+
+  fetch('https://green-r.be/api/stats.php?position=ALL')
       .then((response) => response.json())
       .then((responseJson) => {
 
+
+      
+
         this.setState({
           isLoading: false,
-          dataSource: responseJson,
+                     
         }, function(){
+            const dataCaught = responseJson;
+            const dataLength = Object.keys(dataCaught).length;
+
+            this.setState({
+              dataLength: dataLength,
+              dataCaught: dataCaught,
+            })
+
+            if (this._isMounted) {
+              this.setState({isLoading: false})
+            }
+
 
         });
-
       })
       .catch((error) =>{
-        console.error(error);
+        console.warn(error);
       });
-  }
-  renderFavori = () => {
-    return (
-            <View style={styles.container}>
-                <Text style={styles.valueText}>
-                    Sélectionner une boxe favorite
-                </Text>
-                <RadioGroup radioButtons={this.state.box} onPress={this.onPress} />
-            </View>
-        
-    
-    )
-  }
 }
-  const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    valueText: {
-        fontSize: 18, 
-        marginBottom: 50,
-    },
-});
+
+    constructAPIdata= () => {
+    
+      this.catchAPIdata();
+     
+    var marker=[];
+     for (let index = 0; index < this.state.dataCaught.length; index++) {
+      console.log(index);
+      var element = this.state.dataCaught[index];
+      }
+      return (
+        <></>
+      );
+    } 
+  
+    renderFavori = () => {
+      return (
+              <View style={styles.container}>
+                  <Text style={styles.valueText}>
+                      Sélectionner une boxe favorite
+                  </Text>
+                  <RadioGroup radioButtons={this.state.box} onPress={this.onPress} />
+              </View>
+          
+      
+      )
+    }
+
+  }
+
+
+
+    const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+      },
+      valueText: {
+          fontSize: 18, 
+          marginBottom: 50,
+      },
+  });
