4236ed87a089e04d14b4c9c13da037e4dd094242
Vanden Herrewegen Guillaume <32937851+guivdh@users.noreply.github.com>
2019-12-04 18:07:53 +0100

    Update main.js

--- Web app/include/main.js
+++ Web app/include/main.js
@@ -6,17 +6,3 @@ function afficherForm(contenu){
     document.querySelector(contenu).style.display = "inline-block";
     ancienCont=contenu;
 }
-
-function afficher3d(){
-    if(estAffiche==false) {
-        document.querySelector('.frame3d').style.display = "inline";
-        document.querySelector('.bouton3d').innerHTML = "Masquer";
-        estAffiche=true;
-    }
-    else{
-        document.querySelector('.frame3d').style.display = "none";
-        document.querySelector('.bouton3d').innerHTML = "Visualiser en 3d";
-        estAffiche=false;
-    }
-}
-
